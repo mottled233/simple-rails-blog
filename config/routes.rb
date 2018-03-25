97e9c91afc10093435595de24cc7961b8a8e995c
Lianghuixin <mottled233@sohu.com>
2018-03-25 11:43:25 +0000

    learned layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get '/signup', to:'users#new'
 
-  get 'static_pages/help'
+  get '/home', to:'static_pages#home'
+
+  get '/help', to:'static_pages#help'
   
-  get 'static_pages/about'
+  get '/about', to:'static_pages#about'
   
-  get 'static_pages/contact'
+  get '/contact', to:'static_pages#contact'
   
   root 'static_pages#home'
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,35 +2,35 @@ require '/home/ubuntu/workspace/sample_app/test/test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
     assert_select "title", "Home #{@head_string}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help #{@head_string}"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About #{@head_string}"
   end
   
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact #{@head_string}"
   end
     
   def setup
-    @head_string = "| Sample App use Ruby on Rails"
+    @head_string = "| Sample Blog use Ruby on Rails"
   end
   
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
     assert_select "title", "Home #{@head_string}"
   end
